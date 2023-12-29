8a5f341b97588a466748f4cd8bf90f55aa4fc9c4
OHMINSEOK <116791454+oohminseok@users.noreply.github.com>
2023-12-29 22:34:05 +0900

    [level unrated] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int age) 
+{
+    
+    string answer = std::to_string(age);
+    
+    for(int i=0; i<answer.size(); ++i)
+    {
+        answer[i]=answer[i]+49;
+    }
+    
+    return answer;
+}
