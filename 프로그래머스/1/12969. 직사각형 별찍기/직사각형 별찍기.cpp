6b8a7f73457b887b4ceb4c3e617618b697bd4d6d
OHMINSEOK <116791454+oohminseok@users.noreply.github.com>
2024-02-16 13:35:19 +0900

    [level 1] Title: 직사각형 별찍기, Time: 3.80 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) 
+{
+    int a;
+    int b;
+    cin >> a >> b;
+    
+    for(int i=1; i<=b; ++i)
+    {
+        for(int j=1; j<=a; ++j)
+        {
+             cout << '*';
+        }
+        std::cout<<'\n';
+    }
+
+    return 0;
+}
